bffea1b214b21fd261cc0fc54c35517958ee5cf5
Andrew Coleburn <arcoleburn@gmail.com>
2020-10-20 12:00:35 -0400

    added sort by app

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
-/*eslint-disable */ 
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const playStore = require('./playstore')
+/*eslint-disable */
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const playStore = require("./playstore");
 
+console.log(playStore);
+const app = express();
 
-console.log(playStore)
-const app = express()
+app.use(morgan("dev"));
 
-app.use(morgan('dev'))
+app.use(cors());
 
-app.use(cors())
+function handleGetApps(req, res) {
+  const sort = req.query.sort;
+  console.log(sort)
+  let results = playStore;
+  if (sort) {
+    if (sort !== "rating" && sort !== "app") {
+      return res
+        .status(400)
+        .json({ error: "Can only sort by 'app' and 'rating'" });
+    }
+    if (sort === "app") {
+      results = results.sort((a,b) =>{ 
+        let textA=a.App.toLowerCase()
+        let textB=b.App.toLowerCase()
+        return (textA < textB) ? -1 : (textA> textB) ? 1: 0
+      });}
+      console.log("results after sort:", results)
+    }
+  
+  res.json(results)
+}
+app.get("/apps", handleGetApps);
 
-app.get('/apps', handleGetApps)
+const PORT = 8000;
 
-
-
-const PORT = 8000
-
-app.listen(PORT, () =>{
-    console.log(`server listening at http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server listening at http://localhost:${PORT}`);
+});
